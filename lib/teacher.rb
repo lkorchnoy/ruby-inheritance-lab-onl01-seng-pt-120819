f06d1d7ba93b338ee5ffb5c0e9fb63a8a309bc03
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-02-03 02:37:38 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,18 @@
-class Teacher
-
-  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
+class Teacher < User
   
+  
+  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+  
+  def initialize
+    super
+  end
+
+  def teach
+    knowledge = KNOWLEDGE
+    knowledge.select {|knowledge| knowledge == student.knowldege }
+    knowldege.join(" , ") 
+  end
+  
+    
+
 end
